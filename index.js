6d2a15448349c8c265b02afbba507a9a13997fed
Trung Duong <trungdt.cmaxx@gmail.com>
2022-12-06 08:41:44 +0700

    update, delete

--- index.js
+++ index.js
@@ -53,6 +53,42 @@ app.post("/books/create", (req, res) => {
     });
 });
 
+app.get("/books", (req, res) => {
+    const sql = "SELECT * FROM books";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        res.render("show", { books: result });
+    });
+})
+
+app.get("/books/:id/delete", (req, res) => {
+    const idBook = req.params.id;
+    console.log(idBook);
+    const sql = "DELETE FROM books WHERE id =" + idBook;
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        res.redirect('/books')
+    });
+})
+app.get("/books/:id/update", (req, res) => {
+    const idBook = req.params.id;
+    const sql = "SELECT * FROM books WHERE id = " + idBook;
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+        res.render('update', { book: results[0] });
+    });
+})
+app.post("/books/:id/update", (req, res) => {
+    const idBook = req.params.id;
+    const sql = `UPDATE books SET name = ?, price = ?, author = ?, status = ? WHERE id = ?`;
+    const { name, price, status, author } = req.body;
+    const value = [name, price, author, status, idBook];
+    connection.query(sql, value, (err, results) => {
+        if (err) throw err;
+        res.redirect('/books');
+    });
+})
+
 app.listen(PORT, () => {
     console.log("Server running on port:" + PORT);
 });
